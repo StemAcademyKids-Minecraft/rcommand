a371f0850f49770ec14d48f39ae333dfe0929bab
x64neco <yatoneco0015@gmail.com>
2023-07-14 16:40:34 +0900

    0.1.13

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,3 @@ pub mod mmacro;
 pub mod sound;
 pub mod item;
 pub mod commands;
-use commands::mcommand;
-use sound::mplaysound;
-use commands::mdelay;
--- src/mmacro.rs
+++ src/mmacro.rs
@@ -1,19 +1,19 @@
 
 #[macro_export]
 macro_rules! command {
-    ($e:expr) => { $crate::mcommand($e.to_string())};
-    ($fmt:expr, $($arg:tt)*) => { $crate::mcommand(format!($fmt, $($arg)*))};
+    ($e:expr) => { $crate::commands::mcommand($e.to_string())};
+    ($fmt:expr, $($arg:tt)*) => { $crate::commands::mcommand(format!($fmt, $($arg)*))};
 }
 #[macro_export]
 macro_rules! delay {
     ($e:expr) => {
-        $crate::mdelay($e)
+        $crate::commands::mdelay($e)
     };
 }
 
 
 #[macro_export]
 macro_rules! playsound {
-    ($e:expr) => { $crate::mplaysound($e)};
+    ($e:expr) => { $crate::sound::mplaysound($e)};
     ($fmt:expr, $($arg:tt)*) => { $crate::mplaysound(format!($fmt, $($arg)*))};
 }
